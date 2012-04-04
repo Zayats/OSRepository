c40980cfd41a386b7f50ff3b5d424d727b2d753b
Zayats <zayats.s.v@gmail.com>
2012-04-05 01:06:30 +0600

    1 half-year

---
+++ 1/sort.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+void add(char *name, vector<int> &res)
+{
+	FILE *in = fopen(name, "r");
+	if (in == NULL)
+	{
+		fprintf(stderr, "can't open file %s\n", name);
+		return;
+	}
+
+	int num;
+	try
+	{
+		while (fscanf (in, "%d", &num) > 0)
+		{
+			res.push_back(num);
+		}
+		if (!feof(in))
+		{
+			fprintf(stderr, "file %s contains non digit\n", name);
+		}
+
+	}
+	catch (exception e)
+	{
+		fprintf(stderr, "something wrong with vectors\n");
+	}
+
+	if (fclose(in))
+	{
+		fprintf(stderr, "can't close file %s \n", name);
+	}
+}
+
+
+int main(int argv, char* args[])
+{
+	vector<int> numbers;
+	if (argv < 2)
+	{
+		fprintf(stderr, "Too few arguments");
+		return 0;
+	}
+	for (int i = 1; i < argv; ++i)
+	{
+		add(args[i], numbers);
+	}
+	sort(numbers.begin(), numbers.end());
+	for (vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it)
+	{
+		printf ("%d ", *it);
+	}
+	return 0; 
+}
---
+++ 2/find.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+void BootSector (long pos, FILE* in)
+{
+	char Buffer[512];
+	char filename[50];
+	sprintf(filename, "file%ld", pos);
+
+	FILE *out = fopen(filename, "wb");
+	if (out == NULL)
+	{
+		cerr<<"Error fopen."<<endl;
+		return;
+	}
+
+	if (fseek(in, pos, SEEK_SET))
+	{
+		cerr<<"Error fseek."<<endl;
+		return;
+	}
+
+	if (fread(Buffer, 1, 512, in) != 512)
+	{
+		cerr<<"Error fread."<<endl;
+		return;
+	}
+	if (fwrite(Buffer, 1, 512, out) != 512)
+	{
+		cerr<<"Error fwrite."<<endl;
+		return;
+	}
+	if(ferror(out))
+	{
+		cerr<<"Error fwrite."<<endl;
+		return;
+	}
+
+	if(fclose(out))
+	{
+		cerr<<"Error fclose."<<endl;
+		return;
+	}
+}
+int main ()
+{
+	vector <long> Offset; 
+	FILE *in = fopen("plan9", "rb");
+	if(in == NULL)
+	{
+		cerr<<"Error fopen."<<endl;
+		return 0;
+	}
+	//0x55 0xaa
+	char s, p, byte;
+	int i = 1, begin;
+
+	p = fgetc(in);
+	if(ferror(in))
+	{
+		cerr<<"Error fgetc."<<endl;
+		return 0;
+	}
+	try
+	{
+		while (!feof(in))
+		{
+			s = fgetc(in);
+			if(ferror(in))
+			{
+				cerr<<"Error fgetc."<<endl;
+				return 0;
+			}
+			++i;
+			if(s == char(0xAA) && p == char(0x55))
+			{
+				if(i >= 512)
+				{
+					begin = i - 512;
+						Offset.push_back(begin);
+				}
+
+			}
+			p = s;
+		}
+	}
+	catch(exception e)
+	{
+		cerr<<"Not enougth."<<endl;
+		return 0;	
+	}
+	vector <long> :: iterator it;  
+	for(it = Offset.begin(); it < Offset.end(); ++it)
+	{
+		BootSector(*it, in);
+	}
+	if(fclose(in))
+	{
+		cerr<<"Error fclose."<<endl;
+	}
+	return 0;
+}
